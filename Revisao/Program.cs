5bb3dbf85fb6671652e70d0418cf42de05d8001a
Carlosaacf <carlosaacf@gmail.com>
2021-09-16 05:20:38 -0300

    inclusao do projeto revisao

---
+++ Revisao/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.whiteLine("Informe a opção desejada:");
+            console.whiteLine("1 - Inserir novo aluno");
+            console.whiteLine("2 - Listar alunos");
+            console.whiteLine("3 - Calcular media geral");
+            console.whiteLine("X - Sair");
+            console.whiteLine();
+
+            string opcaoUsuario = console.ReadLine();
+            
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1";
+
+                    break;
+                    
+                    case "2";
+
+                    break;
+
+                    case "3";
+
+                    break;
+
+                    default:
+                    throw new ArgumentOutofRanger(Exception();
+                }
+
+                console.whiteLine("Informe a opção desejada:");
+                console.whiteLine("1 - Inserir novo aluno");
+                console.whiteLine("2 - Listar alunos");
+                console.whiteLine("3 - Calcular media geral");
+                console.whiteLine("X - Sair");
+                console.whiteLine();
+
+                opcaoUsuario = console.ReadLine();
+            }
+
+                       
+                 
+        }
+    }
+}
